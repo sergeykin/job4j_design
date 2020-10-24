1faf1d58a7500ad6c8bdb8036a8c4fe02012ba6b
sega <sergeykinn@mail.ru>
2020-10-24 15:05:17 +0300

    5. Валидация параметров запуска. [#363425]

--- chapter_002/src/main/java/ru/job4j/io/Search.java
+++ chapter_002/src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,8 +9,13 @@ import java.util.List;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, "js").forEach(System.out::println);
+
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Root folder is null or extension is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+
+        Path start = Paths.get(args[0]);
+        search(start, args[1]).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, String ext) throws IOException {
