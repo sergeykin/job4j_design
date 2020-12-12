07fabb618c2c8ac9696ffd0e4df554d388377f68
sega <sergeykinn@mail.ru>
2020-12-12 17:51:46 +0300

    Реализации кеша на SoftReference [#363488]

---
+++ chapter_004/src/main/java/SoftCachedFileProcessor.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.IOException;
+import java.lang.ref.SoftReference;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SoftCachedFileProcessor {
+    private Map<String, List<String>> cache;
+    private SoftReference<Map<String, List<String>>> softReference;
+
+    public SoftCachedFileProcessor() {
+        this.cache = new HashMap<>();
+        this.softReference = new SoftReference<>(cache);
+    }
+
+    public List<String> getFile(String fileName) throws IOException {
+        if (cache.containsKey(fileName)) {
+            return softReference.get().get(fileName);
+        } else {
+            List<String> list = Files.readAllLines(Paths.get(fileName));
+            softReference.get().put(fileName, list);
+            return list;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("names.txt");
+        File fileadr = new File("Address.txt");
+        SoftCachedFileProcessor softCachedFileProcessor = new SoftCachedFileProcessor();
+        System.out.println(softCachedFileProcessor.getFile(file.getPath()));
+        System.out.println(softCachedFileProcessor.getFile(fileadr.getPath()));
+        System.out.println(softCachedFileProcessor.getFile(file.getPath()));
+    }
+}
