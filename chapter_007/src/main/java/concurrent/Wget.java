afa20591653ba444adaf39b7fc082ba942b78c99
sega <sergeykinn@mail.ru>
2021-01-26 18:43:59 +0300

    1.2. Режим ожидания. [#456829]

---
+++ chapter_007/src/main/java/concurrent/Wget.java
@@ -0,0 +1,22 @@
+package concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        System.out.println("Start loading ... ");
+                        for (int i = 0; i < 100; i++) {
+                            System.out.print("\rLoading : " + i  + "%");
+                            Thread.sleep(1000);
+                        }
+                        System.out.println("\rLoading :Loaded.");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        thread.start();
+        System.out.println("Main");
+    }
+}
