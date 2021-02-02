cbe446fcb80bb4a2fd3bca219804233c0452b424
sega <sergeykinn@mail.ru>
2021-02-02 19:24:44 +0300

    2. JCIP. Настройка библиотеки [#456841]

---
+++ chapter_007/src/main/java/cnt/Count.java
@@ -0,0 +1,18 @@
+package cnt;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public class Count {
+    @GuardedBy("this")
+    private int value;
+
+    public synchronized void increment() {
+        this.value++;
+    }
+
+    public synchronized int get() {
+        return this.value;
+    }
+}
