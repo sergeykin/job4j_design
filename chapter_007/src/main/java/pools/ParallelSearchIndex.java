933af3f316b7802f55ecbc9d0a3aa24fc5e0b74c
sega <sergeykinn@mail.ru>
2021-03-05 18:53:24 +0300

    3. ForkJoinPool [#456818]

---
+++ chapter_007/src/main/java/pools/ParallelSearchIndex.java
@@ -0,0 +1,54 @@
+package pools;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+public class ParallelSearchIndex extends RecursiveTask<Integer> {
+    private final Object[] objects;
+    private final int from;
+    private final int to;
+    private final int searchIndex;
+
+    public ParallelSearchIndex(Object[] objects, int from, int to, int searchIndex) {
+        this.objects = objects;
+        this.from = from;
+        this.to = to;
+        this.searchIndex = searchIndex;
+    }
+
+    @Override
+    protected Integer compute() {
+        if ((to - from) < 10) {
+            return linearSearch();
+        }
+        int mid = (from + to) / 2;
+        // создаем задачи для сортировки частей
+        ParallelSearchIndex leftSearch = new ParallelSearchIndex(objects, from, mid, searchIndex);
+        ParallelSearchIndex rightSearch = new ParallelSearchIndex(objects, mid + 1, to, searchIndex);
+        // производим деление.
+        // оно будет происходить, пока в частях не останется по одному элементу
+        leftSearch.fork();
+        rightSearch.fork();
+        // объединяем полученные результаты
+        int left = leftSearch.join();
+        int right = rightSearch.join();
+        return left != -1? left: right;
+    }
+
+    private Integer linearSearch() {
+        if (searchIndex >= from && searchIndex <= to) {
+            return searchIndex;
+        }
+        return -1;
+    }
+
+    public static Integer search(Object[] array, int searchIndex) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        return forkJoinPool.invoke(new ParallelSearchIndex(array, 0, array.length - 1, searchIndex));
+    }
+
+    public static void main(String[] args) {
+        Object[] objects = new Object[100];
+        System.out.println(ParallelSearchIndex.search(objects,99));
+    }
+}
